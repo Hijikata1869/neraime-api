2e8f96360523f4632df99971bc567b53ebe82ece
Hijikata1869 <liv.knk@gmail.com>
2024-05-28 14:11:37 +0900

    フロンをエンドから受け取った店名で特定の店舗情報を取得する処理を追加#9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post 'auth', to: 'auth#create'
+      get 'current_user', to: 'auth#current_user'
+      get 'stores/show_by_name', to: 'stores#show_by_name'
       resources :users, except: :index do
         get 'crowdedness_list', on: :member
       end
@@ -8,8 +11,6 @@ Rails.application.routes.draw do
         get 'crowdedness_list', on: :member
       end
       resources :crowdednesses, except: [:index, :show, :update]
-      post 'auth', to: 'auth#create'
-      get 'current_user', to: 'auth#current_user'
     end
   end
 end
