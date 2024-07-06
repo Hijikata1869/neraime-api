17b0c05b2101fa9598f50383ad99a1222a7bbd76
Hijikata1869 <liv.knk@gmail.com>
2024-07-06 20:24:57 +0900

    最新の投稿を取得してフロントエンドに返すメソッドをcrowdedness_controllerに実装#26

--- app/controllers/api/v1/crowdednesses_controller.rb
+++ app/controllers/api/v1/crowdednesses_controller.rb
@@ -21,6 +21,18 @@ module Api
         end
       end
 
+      def formatted_latest_crowdednesses_list
+        latest_crowdedness = Crowdedness.includes(:user, :store).order(created_at: :desc).limit(3)
+        formatted_latest_crowdedness = latest_crowdedness.map do |crowdedness|
+          crowdedness.attributes.merge('nickname' => crowdedness.user.nickname, 'store_name' => crowdedness.store.name)
+        end
+        if formatted_latest_crowdedness
+          render json: { latest_crowdedness: formatted_latest_crowdedness }, status: :ok
+        else
+          render json: {}, status: 404
+        end
+      end
+
       private
       def crowdedness_create_params
         params.require(:crowdedness).permit(:user_id, :store_id, :day_of_week, :time, :level, :memo)
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get 'stores/show_by_name', to: 'stores#show_by_name'
       get 'stores/show_by_address', to: 'stores#show_by_address'
       get 'stores/show_by_prefecture_name', to: 'stores#show_by_prefecture_name'
+      get 'crowdedness/formatted_latest_crowdednesses_list', to: 'crowdednesses#formatted_latest_crowdednesses_list'
       resources :users, except: :index do
         get 'crowdedness_list', on: :member
         get 'formatted_crowdedness_list', on: :member
