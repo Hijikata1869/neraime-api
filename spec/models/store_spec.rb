4344f17857bc6b5db843f43a9109a47d9ccf38ee
Hijikata1869 <liv.knk@gmail.com>
2024-08-09 19:36:59 +0900

    storeモデルのテストを作成

---
+++ spec/models/store_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe Store, type: :model do
+
+  before do
+    @prefecture = Prefecture.create(
+      name: "宮城県",
+    )
+  end
+
+  it "name, address, prefecture_idがあれば有効な状態であること" do
+    store = Store.new(
+      name: "store",
+      address: "store_address",
+      prefecture_id: @prefecture.id,
+    )
+    expect(store).to be_valid
+  end
+
+  it "nameが存在しなければ無効な状態であること" do
+    store = Store.new(name: nil)
+    store.valid?
+    expect(store.errors[:name]).to include("can't be blank")
+  end
+
+  it "addressが存在しなければ無効な状態であること" do
+    store = Store.new(address: nil)
+    store.valid?
+    expect(store.errors[:address]).to include("can't be blank")
+  end
+
+  it "nameの重複は無効な状態であること" do
+    Store.create(
+      name: "store",
+      address: "store1_address",
+      prefecture_id: @prefecture.id
+    )
+    store = Store.new(
+      name: "store",
+      address: "store2_address",
+      prefecture_id: @prefecture.id
+    )
+    store.valid?
+    expect(store.errors[:name]).to include("has already been taken")
+  end
+
+  it "addressの重複は無効な状態であること" do
+    Store.create(
+      name: "store1",
+      address: "store1_address",
+      prefecture_id: @prefecture.id
+    )
+    store = Store.new(
+      name: "store2",
+      address: "store1_address",
+      prefecture_id: @prefecture.id
+    )
+    store.valid?
+    expect(store.errors[:address]).to include("has already been taken")
+  end
+
+end
