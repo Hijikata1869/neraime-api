5e847f3db3cba9a6b6af91740a0e56367c6fe28e
Hijikata1869 <liv.knk@gmail.com>
2024-08-14 16:20:46 +0900

    store_specにFactory_botを導入#36

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:owner] do
     nickname { "user" }
     sequence(:email) { |n| "user#{n}@example.com" }
     password { "password" }
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -2,64 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Store, type: :model do
 
-  before do
-    @prefecture = Prefecture.create(
-      name: "宮城県",
-    )
-  end
-
   describe "バリデーションのテスト" do
 
     context "有効なバリデーション" do
       it "name, address, prefecture_idがあれば有効な状態であること" do
-        store = Store.new(
-          name: "store",
-          address: "store_address",
-          prefecture_id: @prefecture.id,
-        )
-        expect(store).to be_valid
+        expect(FactoryBot.build(:store)).to be_valid
       end
     end
 
     context "無効なバリデーション" do
       it "nameが存在しなければ無効な状態であること" do
-        store = Store.new(name: nil)
+        store = FactoryBot.build(:store, name: nil)
         store.valid?
         expect(store.errors[:name]).to include("can't be blank")
       end
 
       it "addressが存在しなければ無効な状態であること" do
-        store = Store.new(address: nil)
+        store = FactoryBot.build(:store, address: nil)
         store.valid?
         expect(store.errors[:address]).to include("can't be blank")
       end
 
       it "nameの重複は無効な状態であること" do
-        Store.create(
-          name: "store",
-          address: "store1_address",
-          prefecture_id: @prefecture.id
-        )
-        store = Store.new(
-          name: "store",
-          address: "store2_address",
-          prefecture_id: @prefecture.id
-        )
+        FactoryBot.create(:store, name: "store")
+        store = FactoryBot.build(:store, name: "store")
         store.valid?
         expect(store.errors[:name]).to include("has already been taken")
       end
 
       it "addressの重複は無効な状態であること" do
-        Store.create(
-          name: "store1",
-          address: "store1_address",
-          prefecture_id: @prefecture.id
-        )
-        store = Store.new(
-          name: "store2",
-          address: "store1_address",
-          prefecture_id: @prefecture.id
-        )
+        FactoryBot.create(:store, address: "address")
+        store = FactoryBot.build(:store, address: "address")
         store.valid?
         expect(store.errors[:address]).to include("has already been taken")
       end
