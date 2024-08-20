158d591da9ed36c9e6e7f64cc6dd0bcbf4b7a0cd
Hijikata1869 <liv.knk@gmail.com>
2024-08-20 18:34:21 +0900

    users_api_spec.rbを修正#36

--- spec/requests/users_api_spec.rb
+++ spec/requests/users_api_spec.rb
@@ -6,32 +6,62 @@ RSpec.describe "UsersApis", type: :request do
     @user = FactoryBot.create(:user)
   end
 
-  it "ログイン中のユーザーであれば登録情報を更新できること" do
-    token = generate_token(@user.id)
-    patch "/api/v1/users/#{@user.id}",
-    params: {
-      nickname: "updated_nickname",
-      email: "updatedemail@example.com"
-    }.to_json,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: token
-    }
-    expect(response).to have_http_status(:success)
-  end
+  describe "登録情報の更新" do
+    context "ログイン中のユーザー" do
+      it "ログイン中のユーザーは自分の登録情報を更新できること" do
+        token = generate_token(@user.id)
+        patch "/api/v1/users/#{@user.id}",
+        params: {
+          nickname: "updated_nickname",
+          email: "updatedemail@example.com"
+        }.to_json,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token
+        }
+        expect(response).to have_http_status(:success)
+      end
 
-  it "ログイン中のユーザーは自分のアカウントを削除できること" do
-    token = generate_token(@user.id)
-    delete "/api/v1/users/#{@user.id}",
-    params: {
-      nickname: "updated_nickname",
-      email: "updatedemail@example.com"
-    }.to_json,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: token
-    }
-    expect(response).to have_http_status(:success)
-  end
+      it "ログイン中のユーザーは自分のアカウントを削除できること" do
+        token = generate_token(@user.id)
+        delete "/api/v1/users/#{@user.id}",
+        params: {
+          nickname: "updated_nickname",
+          email: "updatedemail@example.com"
+        }.to_json,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token
+        }
+        expect(response).to have_http_status(:success)
+      end
+    end
 
+    context "非ログインユーザー" do
+      it "ログインしていなければ自分の登録情報を更新できないこと" do
+        patch "/api/v1/users/#{@user.id}",
+        params: {
+          nickname: "updated_nickname",
+          email: "updatedemail@example.com"
+        }.to_json,
+        headers: {
+          "Content-Type": "application/json",
+        }
+        expect(response).to_not have_http_status(:success)
+      end
+
+      it "ログインしていなければ自分のアカウントを削除できないこと" do
+        delete "/api/v1/users/#{@user.id}",
+        params: {
+          nickname: "updated_nickname",
+          email: "updatedemail@example.com"
+        }.to_json,
+        headers: {
+          "Content-Type": "application/json",
+        }
+        expect(response).to_not have_http_status(:success)
+      end
+    end
+
+  end
 end
