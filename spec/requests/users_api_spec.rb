a53d9589d55008b910111091d0d0e3f55583217d
Hijikata1869 <liv.knk@gmail.com>
2024-10-31 21:15:54 +0900

    ゲストユーザーの更新、削除に関するテストを追加#56

--- spec/requests/users_api_spec.rb
+++ spec/requests/users_api_spec.rb
@@ -63,5 +63,40 @@ RSpec.describe "UsersApis", type: :request do
       end
     end
 
+    context "ゲストユーザー" do
+      before do
+        @guest_user = User.create(nickname: "guest", email: "guest@example.com", password: "guest_password", password_confirmation: "guest_password")
+      end
+
+      it "ゲストユーザーの情報は更新できないこと" do
+        token = generate_token(@guest_user.id)
+        patch "/api/v1/users/#{@guest_user.id}",
+        params: {
+          nickname: "guest_updated",
+          email: "guest_updated@example.com"
+        }.to_json,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token
+        }
+        expect(response).to_not have_http_status(:success)
+      end
+
+      it "ゲストユーザーは削除できないこt" do
+        token = generate_token(@guest_user.id)
+        delete "/api/v1/users/#{@guest_user.id}",
+        params: {
+          nickname: "guest",
+          email: "guest@example.com"
+        }.to_json,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token
+        }
+        expect(response).to_not have_http_status(:success)
+      end
+
+    end
+
   end
 end
